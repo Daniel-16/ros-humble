a86b69a1543455069437b606efd9336ca9c28613
Daniel Toba <danieloloruntoba681@gmail.com>
2025-10-07 21:37:03 +0000

    feat: initialize python_parameters package with basic structure and licensing

---
+++ src/python_parameters/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'python_parameters'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Daniel Toba',
+    maintainer_email='danieloloruntoba681@gmail.com',
+    description='Python Parameters',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'minimal_param_node = python_parameters.python_parameters_node:main',
+        ],
+    },
+)
